fd2ac3fb7f2114e6fce9dff22d20525d4bcb2b2d
devenderbutani21 <devenderbutani21@gmail.com>
2023-09-15 11:23:19 -0400

    Secuirty Implemented

--- src/main/java/com/myproject/FoodAPI/configurations/CorsConfig.java
+++ src/main/java/com/myproject/FoodAPI/configurations/CorsConfig.java
@@ -1,19 +1,49 @@
 package com.myproject.FoodAPI.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Bean
+    public UserDetailsManager userDetailManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/pizzaoptions").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/pizzaoptions/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/pizzaoptions").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/pizzaoptions").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/pizzaoptions/**").hasRole("ADMIN"));
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        // disable Cross Site Request Forgery (CSRF)
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
